eee3f03d69cbce0eb327c1d2971f4813bc941679
xuanvu99 <61828496+xuanvu99@users.noreply.github.com>
2021-08-16 17:49:09 +0700

    update new

--- src/app/main/table/table.component.ts
+++ src/app/main/table/table.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import { ConfirmationService } from 'primeng/api';
-import {AppComponent} from "../../app.component";
+import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-table',
@@ -37,7 +35,7 @@ export class TableComponent implements OnInit {
     this.form = this.formBuilder.group({
       tenDanhMuc: ['', [Validators.required]],
       thoiGian: ['', [Validators.required]],
-    })
+    }, { validator: this.uniqueValidator() })
 
     this.loadData();
   }
@@ -57,11 +55,30 @@ export class TableComponent implements OnInit {
     }
   }
 
+  uniqueValidator() {
+    return (formGroup: FormGroup) => {
+      // @ts-ignore
+      let tenDanhMuc = formGroup.controls['tenDanhMuc'];
+
+      if (tenDanhMuc.errors) {
+        return;
+      }
+      const isNameUnique = this.dataSource.map(data => data.tenDanhMuc).some(value => value === tenDanhMuc.value);
+      console.log(isNameUnique)
+      if (isNameUnique) {
+        console.log(isNameUnique);
+        // @ts-ignore
+        tenDanhMuc.setErrors({unique: true});
+      }
+    };
+  }
+
   onSubmit = () => {
     this.submitted = true;
     if (this.form?.invalid) {
       return;
     }
+    this.loadData();
     if (this.addOrEdit) {
       localStorage.setItem('data', JSON.stringify([...this.dataSource, {...this.form.value, trangThai: 0}]));
       this.displayModal = false;
@@ -96,7 +113,7 @@ export class TableComponent implements OnInit {
     localStorage.setItem('data', JSON.stringify(this.dataSource));
   }
 
-  changeStatus = (index:any) => {
+  changeStatus = (index: any) => {
     this.dataSource[index].trangThai = 1;
     localStorage.setItem('data', JSON.stringify(this.dataSource));
   }
